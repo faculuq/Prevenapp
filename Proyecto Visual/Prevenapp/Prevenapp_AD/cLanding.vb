02dc5961bf4ea96c301867645ef0e541d0890d28
faculuq <56322143+faculuq@users.noreply.github.com>
2020-04-21 18:45:02 -0300

    Proyecto visual

---
+++ Proyecto Visual/Prevenapp/Prevenapp_AD/cLanding.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.DataSqlClient
+Imports System.Data
+Imports System.IO
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+Imports System.Configuration
+Public Class cLanding
+
+    Dim oDatabase As Database
+
+    Public Sub New()
+        oDatabase = DatabaseFactory.CreateDatabase("Conn")
+    End Sub
+
+    Public Function Agregar(ByVal Nombre As String, ByVal Apellido As String, ByVal Email As String, ByVal Telefono As String, ByVal Mensaje As String) As Double
+        Return oDatabase.ExecuteScalar("PrevenappMensajesAgregar", Nombre, Apellido, Email, Telefono, Mensaje)
+    End Function
+
+End Class
